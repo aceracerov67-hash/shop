18f6d63ec8db3d4ba681ce847320aa13f69e519a
farzona <aceracerov67@gmail.com>
2025-12-03 21:19:08 +0500

    change one

--- js/plant.js
+++ js/plant.js
@@ -100,3 +100,128 @@ function renderProducts() {
 }
 
 renderProducts();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const cartData = [
+    {
+        name: "Barberton Daisy",
+        sku: "1995751877966",
+        price: 119,
+        qty: 2,
+        img: "./images/image 11.png"
+    },
+    {
+        name: "Blushing Bromeliad",
+        sku: "1995751877065",
+        price: 139,
+        qty: 6,
+        img: "./images/image 15 (1).png"
+    },
+    {
+        name: "Aluminum Plant",
+        sku: "1995751877786",
+        price: 179,
+        qty: 9,
+        img: "./images/image 16.png"
+    }
+];
+
+const cartBody = document.getElementById("cart-body");
+const subtotalEl = document.getElementById("subtotal");
+const totalEl = document.getElementById("total");
+
+// Render function
+function renderCart() {
+    cartBody.innerHTML = "";
+
+    cartData.forEach((item, i) => {
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+            <td class="prod-info">
+                <img src="${item.img}">
+                <div>
+                    <h3>${item.name}</h3>
+                    <span>SKU: ${item.sku}</span>
+                </div>
+            </td>
+
+            <td>$${item.price.toFixed(2)}</td>
+
+            <td>
+                <div class="qty">
+                    <button class="minus" data-index="${i}">-</button>
+                    <span>${item.qty}</span>
+                    <button class="plus" data-index="${i}">+</button>
+                </div>
+            </td>
+
+            <td class="green">$${(item.price * item.qty).toFixed(2)}</td>
+
+            <td><i class="fas fa-trash delete" data-index="${i}"></i></td>
+        `;
+
+        cartBody.appendChild(tr);
+    });
+
+    updateTotals();
+}
+
+// Update totals
+function updateTotals() {
+    let subtotal = 0;
+
+    cartData.forEach(item => {
+        subtotal += item.price * item.qty;
+    });
+
+    subtotalEl.innerText = "$" + subtotal.toFixed(2);
+    totalEl.innerText = "$" + (subtotal + 16).toFixed(2);
+}
+
+// Buttons (+ / - / delete)
+document.addEventListener("click", e => {
+    if (e.target.classList.contains("plus")) {
+        let i = e.target.dataset.index;
+        cartData[i].qty++;
+        renderCart();
+    }
+
+    if (e.target.classList.contains("minus")) {
+        let i = e.target.dataset.index;
+        if (cartData[i].qty > 1) {
+            cartData[i].qty--;
+        }
+        renderCart();
+    }
+
+    if (e.target.classList.contains("delete")) {
+        let i = e.target.dataset.index;
+        cartData.splice(i, 1);
+        renderCart();
+    }
+});
+
+// Start
+renderCart();
